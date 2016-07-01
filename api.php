df0e0254f5d4582adb1646f61dabba52a883b47a
Adonai Cruz <adonai.cruz@gmail.com>
2016-07-01 11:43:27 -0300

    0.1.0

--- api.php
+++ api.php
@@ -1,4 +1,12 @@
 <?php
+	header('Content-Type: application/json');
+	if($_POST['token'] === '62bb61431348e22850828a5829c4373faafe29c1' && $_POST['secret'] === "51a266c2844ccd5cac83d88de88d82d05358aa51" && $_POST['lead']['name'] && $_POST['lead']['email']){
+		$jsonData = json_encode($_POST);
 
-	$response = "{ 'token': '62bb61431348e22850828a5829c4373faafe29c1', 'secret': '51a266c2844ccd5cac83d88de88d82d05358aa51', 'lead':{ 'name':'Front end Ninja', 'email':'frontend.ninja@resultadosdigitais.com.br', 'estado':'SC', 'nível':'Ninja' } }";
-	http_response_code(500);
+		$response = array('success'=>true);
+		http_response_code(200);
+	}else{
+		$response = array('success'=>false);
+		http_response_code(500);
+	}
+	echo json_encode($response);
