4f6476989b1c6aadf719625315bb492ffa1df068
Adonai Cruz <adonai.cruz@gmail.com>
2016-06-30 21:03:32 -0300

    Versao Alpha

---
+++ api.php
@@ -0,0 +1,4 @@
+<?php
+
+	$response = "{ 'token': '62bb61431348e22850828a5829c4373faafe29c1', 'secret': '51a266c2844ccd5cac83d88de88d82d05358aa51', 'lead':{ 'name':'Front end Ninja', 'email':'frontend.ninja@resultadosdigitais.com.br', 'estado':'SC', 'nível':'Ninja' } }";
+	http_response_code(500);
